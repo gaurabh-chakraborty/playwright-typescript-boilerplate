fde850be6f948e16fe1f235662735415fca73a02
achantaprakash <achantas@geekyants.com>
2022-12-19 10:41:17 +0530

    Stickey Header

--- pageFactory/pageRepository/StickyHeader.ts
+++ pageFactory/pageRepository/StickyHeader.ts
@@ -1,15 +1,19 @@
 import { WebActions } from "@lib/WebActions";
 import { StickeyHeaderObjects } from "@objects/StickyHeaderNBObjects";
-import { Page } from "@playwright/test";
+import { Page,Browser,BrowserContext } from "@playwright/test";
 
 let webActions: WebActions;
 
 export class StickyHeader extends StickeyHeaderObjects{
-    readonly page: Page;
+    browser :Browser;
+    context: BrowserContext;
+    page: Page;
 
     constructor(page: Page) {
         super();
         this.page = page;
+        this.context=this.context;
+        this.browser=this.browser;
         webActions = new WebActions(this.page);
     }
 
@@ -20,6 +24,16 @@ export class StickyHeader extends StickeyHeaderObjects{
     
     async NativeBaseLogo():Promise<void>{
         await webActions.clickElement(StickeyHeaderObjects.NativeBase);
-     
+        await this.page.goBack();
+    }
+
+    async Startup(): Promise <void>{
+        const [newWindow] = await Promise.all([
+            this.context.waitForEvent("page"),
+            await webActions.clickElement(StickeyHeaderObjects.startup)
+        ])
+       //  await newWindow.waitForLoadState();
+         await newWindow.close();
+         await this.page.bringToFront();
     }
 }
--- tests/functional/StickeyHeader.test.ts
+++ tests/functional/StickeyHeader.test.ts
@@ -1,6 +1,24 @@
 import test from '@lib/BaseTest';
+import { Browser, BrowserContext, chromium, Page } from '@playwright/test';
 
-test(`Stickey Header`,async({ stickyHeader})=>{
-   await  stickyHeader.NavigateToURL();
-   await stickyHeader.NativeBaseLogo();
+
+test.describe('Stickey',()=>{ 
+   
+   let browser :Browser;
+   let context: BrowserContext;
+   let page: Page;
+   
+   test.beforeAll(async()=>{
+   await page.goto("https://docs.nativebase.io");
 })
+test(`Stickey Header`,async({ stickyHeader})=>{
+  // await  stickyHeader.NavigateToURL();
+   await stickyHeader.NativeBaseLogo();
+   await stickyHeader.Startup();
+})
+
+})
+
+
+
+
