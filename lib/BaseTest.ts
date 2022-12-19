0918eeed941f4b278f8381247993472f73aabddb
achantaprakash <achantas@geekyants.com>
2022-12-19 10:04:18 +0530

    Updated BaseTest.ts

--- lib/BaseTest.ts
+++ lib/BaseTest.ts
@@ -1,19 +1,19 @@
-//<<<<<<< HEAD
+
 import { test as baseTest } from "@playwright/test";
 import { LoginPage } from "@pages/LoginPage";
 import { MyAccountPage } from "@pages/MyAccountPage";
 import { MyAddressesPage } from "@pages/MyAddressesPage";
 import { MyPersonalInformationPage } from "@pages/MyPersonalInformationPage";
 import { GettingstartedPage } from "@pages/GettingStartedPage";
-import { InstallationPage } from "@pages/InstallationPage";
+import { StickyHeader } from "@pages/StickyHeader";
 
 const test = baseTest.extend<{
   loginPage: LoginPage;
   myAccountPage: MyAccountPage;
   myAddressesPage: MyAddressesPage;
   myPersonalInformationPage: MyPersonalInformationPage;
-  gettingStartedPage: GettingstartedPage;
-  installationPage: InstallationPage;
+  gettingStartedpage: GettingstartedPage;
+  stickyHeader: StickyHeader;
 }>({
   loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
@@ -27,45 +27,14 @@ const test = baseTest.extend<{
   myPersonalInformationPage: async ({ page }, use) => {
     await use(new MyPersonalInformationPage(page));
   },
-  gettingStartedPage: async ({ page }, use) => {
+  gettingStartedpage: async ({ page }, use) => {
     await use(new GettingstartedPage(page));
   },
-  installationPage: async ({ page }, use) => {
-    await use(new InstallationPage(page));
-  },
+  stickyHeader: async({page},use)=>{
+    await use(new StickyHeader(page));
+},
 
-  // =======
-  // import { test as baseTest } from '@playwright/test';
-  // import { LoginPage } from '@pages/LoginPage';
-  // import { MyAccountPage } from '@pages/MyAccountPage';
-  // import { MyAddressesPage } from '@pages/MyAddressesPage'
-  // import { MyPersonalInformationPage } from '@pages/MyPersonalInformationPage';
-  // import { StickyHeader } from '@pages/StickyHeader';
 
-  // const test = baseTest.extend<{
-  //     loginPage: LoginPage;
-  //     myAccountPage: MyAccountPage;
-  //     myAddressesPage: MyAddressesPage;
-  //     myPersonalInformationPage: MyPersonalInformationPage;
-  //     stickyHeader: StickyHeader
-
-  // }>({
-  //     loginPage: async ({ page }, use) => {
-  //         await use(new LoginPage(page));
-  //     },
-  //     myAccountPage: async ({ page }, use) => {
-  //         await use(new MyAccountPage(page));
-  //     },
-  //     myAddressesPage: async ({ page }, use) => {
-  //         await use(new MyAddressesPage(page));
-  //     },
-  //     myPersonalInformationPage: async ({ page }, use) => {
-  //         await use(new MyPersonalInformationPage(page));
-  //     },
-  //     stickyHeader: async({page},use)=>{
-  //         await use(new StickyHeader(page));
-  //     }
-  // >>>>>>> e180b4f1c165114bd023405dac3c8d577f6e9a05
 });
 
 export default test;
